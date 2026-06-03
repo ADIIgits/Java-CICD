849fce3ba39e7021a1f4047f9e8cd269a4d14222
ADIIgits <adityalpu27@gmail.com>
2026-06-03 22:13:15 +0530

    first commit

---
+++ src/main/java/com/adi/App.java
@@ -0,0 +1,13 @@
+package com.adi;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Addition:" + add(5,4));
+    }
+}
---
+++ src/test/java/com/adi/AppTest.java
@@ -0,0 +1,22 @@
+package com.adi;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void testAdd(){
+        assertEquals(9,App.add(5,4));
+    }
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
